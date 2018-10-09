d50072804dba515ef0f1947d0a21caa4685ea4ee
Izemir <chistevg.pro@gmail.com>
2018-10-09 22:37:22 +0300

    Simple UI added

---
+++ Assets/CalculatorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatorScript : MonoBehaviour {
+
+    public InputField inputField;
+
+    public Text answer;
+
+    public string input;
+
+	// Use this for initialization
+	void Start () {
+        answer.text = "";
+		
+	}
+	
+	public void GetInput()
+    {
+        input = inputField.text;
+    }
+
+    public void Calculate()
+    {
+        answer.text = input;
+    }
+}
