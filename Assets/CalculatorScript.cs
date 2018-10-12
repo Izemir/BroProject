5d3cc71ba337be14ce72da63ccd13606fe85402d
Izemir <laruscrococat@gmail.com>
2018-10-12 11:19:50 +0300

    Basic parsing

--- Assets/CalculatorScript.cs
+++ Assets/CalculatorScript.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +12,8 @@ public class CalculatorScript : MonoBehaviour {
     public Text answer;
 
     public string input;
+
+    char[] parsing;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +28,40 @@ public class CalculatorScript : MonoBehaviour {
 
     public void Calculate()
     {
-        answer.text = input;
+        parsing = input.ToCharArray();
+
+        //List<string> datalist = new List<string>();
+        //datalist.AddRange(input.Select(c => c.ToString()));
+
+        List<char> datalist = new List<char>();
+        datalist.AddRange(input);
+
+
+
+        answer.text = datalist[2].ToString();
+        //answer.text = datalist.Count.ToString();
+        //answer.text = ParseAndCalc(parsing);
+    }
+
+    public string ParseAndCalc(List<string> toParse)
+    {
+        List<char> result = new List<char>();
+
+
+        int i = 0;
+        while (char.IsDigit(toParse[i]))
+        {
+            result.Add(toParse[i]);
+            i++;
+        }
+
+
+
+        return "Start - "+result.ToString()+"Con";
+    }
+
+    public static bool IsCharDigit(char c)
+    {
+        return ((c >= '0') && (c <= '9'));
     }
 }
